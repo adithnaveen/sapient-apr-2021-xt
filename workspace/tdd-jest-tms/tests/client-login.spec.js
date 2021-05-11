db200ae77b78b1c2eaffd44d57826e621c9f2dec
Naveen <adith.naveen@gmail.com>
2021-05-11 12:42:00 +0530

    updated spec file to have beforeEach

--- workspace/tdd-jest-tms/tests/client-login.spec.js
+++ workspace/tdd-jest-tms/tests/client-login.spec.js
@@ -2,14 +2,14 @@ const {loginClient} = require("../scripts/client-login");
 jest.setTimeout(20000); 
  
 describe('test for client login ', () => {
-    
-    
-    test('should get the jwt when valid credentials are shared',  async () => {
-        var obj={};
-            
+    let obj={};
+    beforeEach(() => {
         obj.email="naveen@naveen.com";
-        obj.password="naveen1234"
+        obj.password="naveen1234";
         obj.pic="/pic.jpg";
+    });
+
+    test('should get the jwt when valid credentials are shared',  async () => {
         let message =  await loginClient(obj); 
         expect(message.message).toEqual("User authorization successful")
     })
