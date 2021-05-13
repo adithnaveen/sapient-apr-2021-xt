feb05f0464c15de32b6822ccbe4227645b3717ab
Naveen <adith.naveen@gmail.com>
2021-05-13 12:30:39 +0530

    add typescript code ver 1

---
+++ workspace/typescript-works/app.js
@@ -0,0 +1,17 @@
+"use strict";
+let mystring = "Hello Wolrd";
+console.log(`MyString is : ${mystring}`);
+let numberVar;
+let boolVar;
+let anyVar;
+let stringVar;
+let numArray = [3443, 65, 6, 7, 8, 8];
+let anyArray = ["hi", 3434, true];
+var COLOR;
+(function (COLOR) {
+    COLOR[COLOR["RED"] = 0] = "RED";
+    COLOR[COLOR["BLUE"] = 1] = "BLUE";
+    COLOR[COLOR["GREEN"] = 2] = "GREEN";
+})(COLOR || (COLOR = {}));
+let myFavColor = COLOR.BLUE;
+console.log(myFavColor);
---
+++ workspace/typescript-works/app.ts
@@ -0,0 +1,19 @@
+// $ tsc --build tsconfig.json
+let mystring:string = "Hello Wolrd";
+ 
+console.log(`MyString is : ${mystring}`);
+
+let numberVar : number; 
+let boolVar: boolean; 
+let anyVar : any ; 
+let stringVar : string
+let numArray:number[] =  [3443,65,6,7,8,8];
+let anyArray:any[] = ["hi", 3434, true]; 
+
+enum COLOR {RED=0, BLUE=1, GREEN=2}
+
+let myFavColor = COLOR.BLUE
+
+console.log(myFavColor);
+
+
