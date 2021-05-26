e612ddf1705b6db64507a2fb476602750341d4cd
Naveen <adith.naveen@gmail.com>
2021-05-26 12:30:16 +0530

    added hoc code

--- workspace/react-works/react-app-01/src/06-hoc/ContactList.js
+++ workspace/react-works/react-app-01/src/06-hoc/ContactList.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import ContactCard from './ContactCard'
+import PropTypes from 'prop-types';
 import Loading from './Loading';
 
-const ContactList = ({contacts}) => {
+const ContactList = ({contacts, name}) => {
     let list = contacts.map(c => <ContactCard contact={c} key ={c.id} />)
-    return <div>{list}</div>
+    return <div>
+        {name}
+        {list}
+        </div>
 }
+
+// you can do type checking 
+ContactList.propTypes = {
+    contacts: PropTypes.array,
+    name:PropTypes.string.isRequired
+};
+
+// assign default values for the properties 
+ContactList.defaultProps ={
+    name:"Harry"
+}
+
 export default Loading('contacts')(ContactList)
--- workspace/react-works/react-app-01/src/06-hoc/index.js
+++ workspace/react-works/react-app-01/src/06-hoc/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-
-import ContactCard from './ContactCard';
+import axios from 'axios';
+import styled from 'styled-components';
 import ContactList from './ContactList';
 class App extends Component {
 
@@ -9,15 +9,49 @@ class App extends Component {
     }
 
     componentDidMount() {
-        fetch("http://localhost:4000/contacts/")
-            .then(resp => resp.json())
-            .then(resp => this.setState({ contacts: resp }))
+        // using fetch 
+        // fetch("http://localhost:4000/contacts/")
+        //     .then(resp => resp.json())
+        //     .then(resp => this.setState({ contacts: resp }))
+
+        // using axios 
+        axios.get("http://localhost:4000/contacts/")
+            .then(resp => {
+                this.setState({ contacts : resp.data});
+            })
+            // .catch(err => console.log(err))
+            // .then(somdata => console.log(somdata));
     }
+
+  
+
     render() {
-       
+         
+     let Title = styled.h1`
+     font-size: 1.5em;
+     text-align: center;
+     color: palevioletred;
+    `;
+
+    let Button = styled.button`
+        background-color:${props => props.primary ? "cyan" :"blue"};
+        color:${props => props.primary ? "black" :"white"};
+        margin:1em;
+        padding: 5px 10px;
+        border-radius:3px;
+    `;
+    let DangerButton = styled(Button)`
+        border-color:red;
+    `;
+
         return (
             <div className="container">
                 <h1 className="alert alert-info">Working with Contacts</h1>
+                <Title>Working with Contacts</Title>
+                <Button>Styled Button</Button>
+                <Button primary >Styled Button</Button>
+                <DangerButton>This is Danger</DangerButton>
+                <DangerButton primary>This is Danger Primary</DangerButton>
                 <ContactList contacts={this.state.contacts} />
             </div>
         );
